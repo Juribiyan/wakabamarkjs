f9f69914991b6aabbf9671471c8c6758d7899b27
Juribiyan <snivy@ponyasha.ru>
2013-08-28 00:14:45 +0600

    Update wakabamark.js

--- wakabamark.js
+++ wakabamark.js
@@ -1,128 +1,114 @@
-//chat
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
-
-io.set('log level', 1);
-io.set('browser client minification', true);  // send minified client
-io.set('browser client etag', true);          // apply etag caching logic based on version number
-
-
-server.listen(process.env.PORT);
-
-app.use(express.favicon('public/images/favicon.ico'));
-app.use('/img', express.static(__dirname + '/public/images'));
-app.use('/etc', express.static(__dirname + '/public/etc'));
-
-var globalOptions = {
-    allowCORS: true,
-    queueing: true,
-    messagestostore: 10,
-    maxRoomNameSize: 12,
+var WM = function() {
+    this.tags = [];
+    
+    this.options = {
+        makeLinks: true,
+        negation: true,
+        escapeHTML: true,
+        greenquoting: true
+    }
+    
+    this.clinks = {
+        exp: /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi,
+        rep: "<a href='$1'>$1</a>"
+    }
+    
+    this.escapechars = [
+        [/\'/g, '&#039;'],
+        [/\"/g, '&quot;'],
+        [/</g, '&lt;'], 
+        [/\>/g, '&gt;'],
+    ];
+    
+    this.negationtags = [
+        ['`','`'],
+        ['[code]','[/code]'],  
+    ];
+    
+	this.newTag = function(pattern, replace) {
+		for (var i = 0; i <= 1; i++) {
+			pattern[i] = pattern[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+		}
+		var exp = new RegExp(pattern[0]+'([\\s\\S]+?)'+pattern[1], "mg");
+        return {exp: exp, rep: replace[0]+"$1"+replace[1]};
+	};
+	
+	this.addTag = function (pattern, replace) {
+	    this.tags.push(this.newTag(pattern, replace));
+	}
+	
+	this.apply = function(str) {
+		var tag = {};
+		
+		if(this.options.negation) {
+		    for (var i = this.negationtags.length - 1; i >= 0; i--) {
+    			tag = this.newTag(this.negationtags[i], ["",""]).exp;
+    			str = str.replace(tag, function(match, p1, offset, s) {
+            	    var tagescapechars = [
+                        [/\*/mg, '&#42;'],
+                        [/_/, '&#95;'],
+                        [/\[/mg, '&#91;'], [/\]/mg, '&#93;'],
+                        [/%/mg, '&#37;'],
+                        [/\>/g, '&#62;'],
+                    ];
+                    for (var j = tagescapechars.length - 1; j >= 0; j--) {
+                        p1 = p1.replace(tagescapechars[j][0], tagescapechars[j][1]);
+                    }
+                    return p1;
+                });
+    		}
+		}
+		
+		for (i = this.escapechars.length - 1; i >= 0; i--) {
+    		str = str.replace(this.escapechars[i][0], this.escapechars[i][1]);
+    	}
+		
+		if(this.options.makeLinks) {
+		    str = str.replace(this.clinks.exp, this.clinks.rep);
+		}
+		
+		if(this.options.greenquoting) {
+		    str = str.replace(
+		        /(?:\&gt;)([^\r\n]+)(?:(?:\r|\n)?)/mg, 
+		        "<span class=\"unkfunc\">\&gt;$1</span>"
+		    );
+		}
+		
+		for (i = this.tags.length - 1; i >= 0; i--) {
+			tag = this.tags[i];
+			str = str.replace(tag.exp, tag.rep);
+		}
+		return str;
+	};
+	
+	this.registerTags = function(tags, destination) {
+		var tag = [];
+		for (var i = tags.length - 1; i >= 0; i--) {
+			tag = tags[i];
+			this.tags.push(this.newTag(tag[0], tag[1]));
+		}
+	};
 };
 
-var roomlist = [
-    {name: "Room 1", isDefault: true, first: true},
-    {name: "Room 2", isDefault: true, first: false},
-], rlist = [];
 
-var lastmessages = {};
+var wm_tags = [
+	[['**','**'],		['<b>','</b>']],
+	[['__','__'],		['<b>','</b>']],
+	[['*','*'],		    ['<i>','</i>']],
+	[['_','_'],		    ['<i>','</i>']],
+];
 
-function refreshRoomlist() {
-    arr_iterate(roomlist, function(room) {
-        rlist.push(room.name);
-    });
-}
+var ku_tags = [
+	[['[b]','[/b]'],	['<b>','</b>']],
+	[['[i]','[/i]'],	['<i>','</i>']],
+	[['[u]','[/u]'],	['<span style="text-decoration: underline">','</span>']],
+	[['[s]','[/s]'],	['<strike>','</strike>']],
+	[['%%','%%'],		['<span class="spoiler">','</span>']],
+	[['[aa]','[/aa]'],	['<span style="font-family: Mona,\'MS PGothic\' !important;">','</span>']]
+];
 
-refreshRoomlist();
 
-function errexit(res, msg) {
-    res.json({
-        status: "error", 
-		errmsg: msg
-	});
-	return false;
-}
+var wm = new WM();
 
-app.get('/', function (req, res) {
-  res.sendfile('public/chat.html');
-});
-
-io.sockets.on('connection', function (socket) {
-    //on connection, send list of available rooms
-    socket.emit('roomlist', {rooms: roomlist});
-    //join some room
-    socket.on("subscribe", function(data) {
-        if(typeof data.to !== "string" || !in_array(data.to, rlist))
-            return socket.emit('log', 'Wrong data passed with Subscribe command');
-        socket.join(data.to);
-        var lmsgs = (globalOptions.queueing)
-            ? lastmessages[data.to]
-            : false;
-        socket.emit('subcribeApproval', {
-            lastmessages: lmsgs,
-            to: data.to
-        });
-    });
-    //leave room
-    socket.on("unsubscribe", function(data) {
-        if(typeof data.from !== "string" || !in_array(data.from, rlist))
-            return socket.emit('log', 'Wrong data passed with Unsubscribe command');
-        socket.leave(data.from);
-    });
-    //receive message
-    socket.on("sendmessage", function(data) {
-        if(typeof data.room !== "string" 
-            || typeof data.message !== "string"
-            || !in_array(data.room, rlist)
-            || data.message.length > 150
-            || data.message.length < 1
-        ) return socket.emit('log', 'Wrong data passed with Message');
-        io.sockets.in(data.room).emit("newmessage", {message: data.message});
-        if(globalOptions.queueing) {
-            if(typeof lastmessages[data.room] === "undefined")
-                lastmessages[data.room] = [];
-            lastmessages[data.room].push(data.message);
-            if(lastmessages[data.room].length >= globalOptions.messagestostore)
-                lastmessages[data.room].shift();
-        }
-    });
-    //user creates new room
-    socket.on("newroom", function(data) {
-        if(typeof data.room !== "string") 
-            return socket.emit('log', 'Wrong data passed with Message');
-        if(in_array(data.room, rlist))
-            return socket.emit('newroomDenial', 'Room already exists');
-        if(data.room.length >= globalOptions.maxRoomNameSize)
-            return socket.emit('newroomDenial', 'Room name must be '+globalOptions.maxRoomNameSize+' or less characters long');
-        roomlist.push({
-            name: data.room,
-            isDefault: false,
-            first: false
-        });
-        refreshRoomlist();
-        io.sockets.emit('newRoomCreated', {name: data.room});
-    });
-});
-
-//useful shit
-
-function in_array(needle, haystack)
-{
-    for(var key in haystack)
-    {
-        if(needle === haystack[key])
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-function arr_iterate(array, callback) {
-    var i=0, len = array.length;
-    for ( ; i < len ; i++ ){
-        callback(array[i]);
-    }
-}
+wm.registerTags(wm_tags);
+wm.registerTags(ku_tags);
